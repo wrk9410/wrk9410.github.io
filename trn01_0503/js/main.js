d083d067881d36a9676fdac8eca22524143073c6
wrk9410 <104603832+wrk9410@users.noreply.github.com>
2022-05-03 17:09:10 +0900

    Add files via upload

---
+++ trn01_0503/js/main.js
@@ -0,0 +1,20 @@
+/* window.addEventListener('DOMCContentLoaded', function () {
+    document.querySelector('.header').style.display = 'none';
+}) */
+
+$(function () {
+    /* {}는 객체가 될 수 있다. */
+    $('.main_slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        vertical: true,
+        dots: true,
+    });
+})
+
+
+
+
+
+
